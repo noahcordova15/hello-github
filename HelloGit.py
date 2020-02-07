422ccd062b6a20eea7d4fca1063fbe7f1fe4c82b
noahcordova15 <noahcordova@amdg.rockhursths.edu>
2020-02-07 08:49:41 -0600

    random

---
+++ HelloGit.py
@@ -0,0 +1,53 @@
+# comment
+print("Hello, World!")
+a = 6
+b = 5
+print (a + b)
+
+# how to make a list
+mylist = [1,2,3]
+
+# how to stride
+print(mylist[0:6:2]) # from index 0 to 6 skip by two
+
+# strings are just lists
+phrase = 'Catch the dog'
+phrase[2] # t
+phrase[4] # h
+phrase[-1:0:-3] #strides work backwards, -1 is right, 0 is left
+
+# how to access elements of a list
+print(myList[1])
+
+# how to write a function
+def sumFunction(a, b):
+	return a + b
+
+# how to call a function
+print(sumFunction(2,20))
+
+# blocks of code
+if(x == 10):
+    x = 5
+elif(y == -10):
+    y = 5
+else:
+    x = y - x
+
+# loop
+mylist = [1, 3, 8, 412, 43, 2, 20]
+
+# length of list
+len(mylist)
+
+# for loop by index
+for i in range(len(mylist))
+    print(mylist[i])
+
+# for each loop
+for val in mylist:
+    print(val)
+
+# dictionaries
+lookup = {}
+lookup['kc'] = 'chiefs'
